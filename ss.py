4408c03ae249cbe44663d817663a99f14a3c3a7d
shivateja-war <shivateja9899@gmail.com>
2026-09-10 10:17:02 +0530

    Add files via upload

--- ss.py
+++ ss.py
@@ -1,18 +1,41 @@
-l1 = [1,3.14,"shiva",True]
-print(l1)
-l2=[2,"abcd",False,6.0]
-print(l2)
-a=3
-b=4.14
-l3=[a,b]
-print(l3)
-c,d,e, = 1,2,3
-print(c)
-print(d)
-print(e)
-l4 = [-1,-3.14,"shiva",True]
-print(l4)
-print(l1[-3])
-l5 = ["😊","😂","🤣","❤️","😍"]
-print(l5)
-#print(range)
+#for i in range(0,10):
+    #if i ==5:
+    #    continue
+   # print(i)
+#for i in range(0,10):
+ #   if i ==5:
+   #     break
+#    print(i)
+for i in range(0,10):
+    if i%2==0:
+        continue
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
